dc09f00eb516b04eed6ca35676ce1148fb6e96d3
Brendan Horng <brendan.horng@ludusamo.com>
2016-04-07 06:42:52 -0400

    Add implementation for string less than

--- C_Compiler/src/string_util.c
+++ C_Compiler/src/string_util.c
@@ -37,5 +37,8 @@ char *str_copy(const char *origin) {
 }
 
 int str_lt(const char *str1, const char *str2) {
+	for (int i = 0; i < strlen(str1); i++) {
+		if (str1[i] < str2[i]) return 1;
+	}
 	return 0;
 }
