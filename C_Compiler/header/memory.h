92763d062a5113e04797502213162596abe72384
Brendan Horng <brendan.horng@ludusamo.com>
2016-06-19 11:47:03 -0400

    Add memory functions for memory management

---
+++ C_Compiler/header/memory.h
@@ -0,0 +1,21 @@
+#ifndef MEMORY_H
+#define MEMORY_H
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "map.h"
+
+Map *global_memory, *local_memory, *function_addresses;
+
+void init_mem();
+void deinit_mem();
+
+int create_global_variable(const char *key, int addr);
+int create_local_variable(const char *key, int offset);
+int create_function(const char *key, int addr);
+
+int get_global_addr(const char *key);
+int get_local_offset(const char *key);
+int get_function_addr(const char *key);
+
+#endif // MEMORY_H
