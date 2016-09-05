6492f79bcf1607e3c655aee7da42462ee61b6765
Brendan Horng <brendan.horng@ludusamo.com>
2016-09-05 15:05:44 -0400

    Add option to specify the bytecode that the VM runs

--- VM/main.c
+++ VM/main.c
@@ -3,61 +3,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*const int program[23] {
-	// FUNCTION A()
-	PUSH, 0,  // int B
+int *program;
+int num_global = 256;
 
-	PUSH, 0,  // int C
+int main(int argc, const char* argv[]) {
+	if (argc == 1) {
+		fprintf(stderr, "Insufficient Arguments\n");
+	} else if (argc == 2) {
+		FILE *bytecode = fopen(argv[1], "r");
+		if (!bytecode) {
+			fprintf(stderr, "File %s Does Not Exist\n", argv[1]);
+			exit(EXIT_FAILURE);
+		}
+		int mainMethod;
+		fscanf(bytecode, "%d", &mainMethod);
+		int programIndex = 0;
+		int byte;
+		program = calloc(sizeof(int), 1024);
+		while (fscanf(bytecode, "%d", &byte) != -1) {
+			program[programIndex++] = byte;
+		}
 
-	PUSH, 2,  //| B = 2
-	STORE, 1, //|
-
-	PUSH, 3,  //| C = 3
-	STORE, 2, //| 
-
-	LOAD, 1,  //| return B + C
-	LOAD, 2,  //|
-	ADDI,     //|
-	RET,      //|
-
-	// MAIN FUNCTION
-	CALL, 0, 0,
-	PRINTI,
-	HALT 
-};*/
-
-const int program[] = {
-	PUSH, 1,
-	GSTORE, 0,
-	GLOAD, 0,
-	PUSH, 1,
-	EQ,
-	BRF, 17,
-	PUSH, 0,
-	PUSH, 0,
-	STORE, 1,
-	POP,
-	PUSH, 0,
-	GSTORE, 1,
-	HALT
-};
-
-int *program1;
-
-int main() {
-	//runProgram(program, 18, 0);
-	FILE *bytecode;
-
-	int mainMethod;
-	fscanf(bytecode, "%d", &mainMethod);
-
-	int programIndex = 0;
-	int byte;
-	while (fscanf(bytecode, "%d", &byte) != -1) {
-		program1[programIndex] = byte;
-		programIndex++;
+		runProgram(program, mainMethod, num_global);
+	} else {
+		fprintf(stderr, "Too Many Arguments\n");
 	}
-
-	runProgram(program1, mainMethod, 3);
 	return 0;
 }
