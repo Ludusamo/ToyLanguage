e2604d2ccab2ad65a01cd2b0bf00cb4d555cc617
Brendan Horng <brendan.horng@ludusamo.com>
2016-09-17 01:54:58 -0400

    Remove redundant loop through local variables in compiler

--- C_Compiler/src/compiler.c
+++ C_Compiler/src/compiler.c
@@ -196,8 +196,8 @@ void compile_const(Linked_List *instructions, ASTNode *const_node) {
 void compile_var(Linked_List *instructions, ASTNode *var_node, int depth) {
 	char *id = GET_AST_STR_DATA(SUB_NODE(var_node, 1));
 	Memory_Address *addr = 0;
-	while (!addr && depth > 0) {
-		addr = get_local_addr(id, depth--);
+	if (depth > 0) {
+		addr = get_local_addr(id, depth);
 	}
 	if (!addr) {
 		add_link(instructions, GLOAD_OP);	
